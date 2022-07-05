9c58a0f8894b9fe351ed1838c400934c5dc59963
Akp-108 <akp1081@gmail.com>
2022-07-05 19:55:42 +0530

    testing

---
+++ src/ApiCall.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+
+const ApiCall = () => {
+    const [data , setData] = useState([])
+    useEffect(()=>{
+        const url = "http://localhost:3001/api/details";
+        fetch(url).then(res => res.json())
+            .then(res => setData(res)  )
+    },[])
+  return (
+    <div>
+        {
+            data.map((res)=>{
+                return(
+                <div>
+                <h1>{res.name}</h1>
+                <h3>{res.title}</h3>
+                <img src={res.img} alt="" />
+                </div>)
+                
+            })
+        }
+    </div>
+  )
+}
+
+export default ApiCall
